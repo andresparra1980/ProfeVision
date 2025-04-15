fdef61ae04848179cc080a030c37a74daddcda2c
Andres Parra <github@andresparra.co>
2025-04-15 08:34:58 -0500

    updated ecosystem file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,6 +5,7 @@ module.exports = {
       script: "node_modules/.bin/next",
       args: "start",
       instances: "max",
+      exec_mode: "cluster",
       autorestart: true,
       watch: false,
       max_memory_restart: "1G",
