07dfa6dc34f9b33664adfb2326f9f86bdba4ddfb
andresparra1980 <github@andresparra.co>
2025-06-26 17:47:24 -0500

    feat(layout): agregar Google Tag Manager para seguimiento de eventos
    - Incluir script de Google Tag Manager en el layout principal para mejorar el seguimiento de eventos y análisis.
    - Añadir iframe de noscript para compatibilidad con navegadores que no soportan JavaScript.
    
    Estos cambios permiten una mejor integración con herramientas de análisis y optimizan la recolección de datos sobre el uso de la aplicación.

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google';
 import '@/styles/globals.css';
 import { ClientLayout } from '@/components/shared/client-layout';
 import { AuthProvider } from '@/components/shared/auth-provider';
+import Script from 'next/script';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -18,10 +19,35 @@ export default function RootLayout({
 }) {
   return (
     <html lang="es" suppressHydrationWarning>
+      <head>
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-5SFVLZMG');
+            `,
+          }}
+        />
+      </head>
       <body
         className={`min-h-screen bg-background font-sans antialiased ${inter.className}`}
         suppressHydrationWarning
       >
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5SFVLZMG"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+        
         <AuthProvider>
           <ClientLayout>{children}</ClientLayout>
         </AuthProvider>
